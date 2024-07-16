1cd215b31ef08ff72af45abdcea1758b22d21eb0
fedeStaffo <fede01staf@gmail.com>
2024-07-16 18:19:42 +0200

    Add test repository

--- src/model/monster.ts
+++ src/model/monster.ts
@@ -2,33 +2,31 @@ import { Model, Column, Table, BelongsTo, ForeignKey, HasMany, DataType } from '
 import { Effect } from './effect';
 import { Session } from './session';
 import { MonsterSkill } from './monster_skill';
-import Entity from './entity';
-
 
 @Table({
   tableName: 'monsters',
   timestamps: true,
 })
-export class Monster extends Model<Monster> implements Entity {
-  @Column declare _name: string;
+export class Monster extends Model<Monster> {
+  @Column declare name: string;
 
-  @Column declare authorUID: string;
+  @Column declare userUID: string;
 
-  @Column declare _maxHp: number;
+  @Column declare maxHp?: number;
 
-  @Column declare _hp: number;
+  @Column declare hp: number;
 
-  @Column declare armorClass: number;
+  @Column declare ac?: number;
 
-  @Column(DataType.ARRAY(DataType.STRING)) declare enchantments: string[];
+  @Column(DataType.ARRAY(DataType.STRING)) declare enchantments?: string[];
 
-  @Column declare isReactionActivable: boolean;
+  @Column declare isReactionActivable?: boolean;
 
-  @Column declare speed: number;
+  @Column declare speed?: number;
 
-  @Column(DataType.ARRAY(DataType.STRING)) declare weapons: string[];
+  @Column(DataType.ARRAY(DataType.STRING)) declare weapons?: string[];
 
-  @Column declare effects?: Effect[];
+  @Column(DataType.STRING) declare effect?: Effect;
 
   @Column(DataType.ARRAY(DataType.STRING)) declare effectImmunities?: Effect[];
 
@@ -42,6 +40,6 @@ export class Monster extends Model<Monster> implements Entity {
   declare skills: MonsterSkill[];
 
   get isDead(): boolean {
-    return this._hp <= 0;
+    return this.hp <= 0;
   }
 }
