a85296d98626052b5232acc77c02a8da39d8b2fb
Valerio Morelli <valeriomorelli50@gmail.com>
2024-07-17 13:21:01 +0200

    Update and reorder according to README api routes;

--- src/api.ts
+++ src/api.ts
@@ -79,114 +79,100 @@ const verifyToken = async (req: RequestWithToken, res: Res, next: NextFunction) 
 app.get('/', requestTime, checkHeader, checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   res.send('Hello World!');
 });
-
 // This endpoint retrieves the user document associated with the UID 
 // of the JWT token from the Firebase Firestore DB. 
 app.get('/user', requestTime, checkHeader, checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   getDocument('users', req.decoded_token!.uid);
   res.send(req.decoded_token!.name);
 });
-
 // This endpoint retrieves a 1 hour JWT from Firebase Auth.
 app.get('/token', requestTime, async (req: RequestWithToken, res: Res) => {
   const token = await signInAndGetIdToken('valeriomorelli50@gmail.com', 'aaaaaa');
   res.send(`Your JWT is ${token}`);
 });
 
-// =========================================================
 
+// Session Routes ==============================================================================
 app.get('/sessions', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   getSessions(req, res);
 });
-
 app.post('/sessions', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   createSession(req, res);
 });
-
 app.get('/sessions/:sessionId', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   getSessionInfo(req, res);
 });
-
 app.delete('/sessions/:sessionId', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   deleteSession(req, res);
 });
-
 app.patch('/sessions/:sessionId/start', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   startSession(req, res);
 });
-
 app.patch('/sessions/:sessionId/pause', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   pauseSession(req, res);
 });
-
 app.patch('/sessions/:sessionId/continue', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   continueSession(req, res);
 });
-
 app.patch('/sessions/:sessionId/stop', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   stopSession(req, res);
 });
 
-app.patch('/sessions/:sessionId/addEntity', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
-  addEntity(req, res);
-});
-
-app.get('/sessions/:sessionId/monsters/:monsterId', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
-  getMonsterInfo(req, res);
-});
-
-app.delete('/sessions/:sessionId/entity', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
-  deleteEntity(req, res);
-});
-
-app.get('/diceRoll', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
-  diceRoll(req, res);
-});
-
+// Turn Routes =================================================================================
 app.get('/sessions/:sessionId/turn', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   getTurn(req, res);
 });
-
 app.patch('/sessions/:sessionId/turn/postpone', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   postponeTurn(req, res);
 });
-
 app.patch('/sessions/:sessionId/turn/end', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   endTurn(req, res);
 });
 
+// Attack Routes ===============================================================================
+app.get('/diceRoll', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
+  diceRoll(req, res);
+});
 app.patch('/sessions/:sessionId/attack', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   makeAttack(req, res);
 });
-
 app.get('/sessions/:sessionId/savingThrow', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   getSavingThrow(req, res);
 });
-
-app.patch('/sessions/:sessionId/addEffect', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
+app.patch('/sessions/:sessionId/effect', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   addEffect(req, res);
 });
-
-app.get('/sessions/:sessionId/:entityId', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
-  getEntityInfo(req, res);
-});
-
-app.patch('/sessions/:sessionId/:entityId', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
-  updateEntityInfo(req, res);
-});
-
 app.patch('/sessions/:sessionId/reaction', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   enableReaction(req, res);
 });
 
+
+// Entity Routes ===============================================================================
+app.patch('/sessions/:sessionId/entities', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
+  addEntity(req, res);
+});
+// app.get('/sessions/:sessionId/monsters/:monsterId', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
+getMonsterInfo(req, res);
+// });
+app.delete('/sessions/:sessionId/entities/:entityId', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
+  deleteEntity(req, res);
+});
+app.get('/sessions/:sessionId/entities/:entityId', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
+  getEntityInfo(req, res);
+});
+app.patch('/sessions/:sessionId/entities/:entityId', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
+  updateEntityInfo(req, res);
+});
+
+// History Routes ==============================================================================
 app.get('/sessions/:sessionId/history', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   getHistory(req, res);
 });
-
 app.post('/sessions/:sessionId/history', checkToken, verifyToken, (req: RequestWithToken, res: Res) => {
   updateHistory(req, res);
 });
 
+// Starts the express server
 (async () => {
   await initializeSequelize();
   app.listen(3000, () => {
