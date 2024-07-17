1de272c7cab02348a668d19f18811de0850497b5
fedeStaffo <fede01staf@gmail.com>
2024-07-17 19:27:51 +0200

    Add getTurnService and postponeTurnService

--- src/service/history_service.ts
+++ src/service/history_service.ts
@@ -1,10 +1,10 @@
-import { AugmentedRequest } from "../api";
+import { AugmentedRequest } from '../api';
 import { Response as Res } from 'express';
 
 export async function getHistoryService(req: AugmentedRequest, res: Res) {
-    // TODO
+  // TODO
 }
 
 export async function updateHistoryService(req: AugmentedRequest, res: Res) {
-    // TODO
+  // TODO
 }
